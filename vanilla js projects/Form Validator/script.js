a24040f07f8099e1450481c37b4ea4cbf3dd0427
Meghu Raval <112463244+meghuraval@users.noreply.github.com>
2023-07-18 15:18:21 -0400

    Add files via upload

---
+++ vanilla js projects/Form Validator/script.js
@@ -0,0 +1,76 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+//functions
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+}
+
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+
+function checkEmail(input) {
+  const re = /\S+@\S+\.\S+/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, "email is not valid");
+  }
+}
+
+function checkRequired(inputArr) {
+  inputArr.forEach(function (input) {
+    if (input.value.trim() === "") {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+function checkPassword(input, input2) {
+  if (!input.value.trim() === input2 || input2.value.trim() === "") {
+    showError(input2, "the two passwords dont match");
+  } else {
+    showSuccess(input2);
+  }
+}
+
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be atleast ${min} characters`
+    );
+  } else if (input.value > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than or equal to ${max} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
+}
+
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+//event listeners
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  checkPassword(password, password2);
+});
